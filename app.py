1462f6378b74655aa17f5b18231c8e9eace0b74a
Simon Zou <simon.zou@gmail.com>
2015-05-02 21:44:48 -0700

    pretty printing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from twython import Twython
+from pprint import pprint
 
 APP_KEY = "Qn4Nx6C9LtoAlKtYLtoNQX2uY"
 APP_SECRET = "mFk56Ec74tSW74Dp8uQqmyxMHKVeMAgQU8cLjRFqV3Ro87cSiN"
@@ -6,4 +7,5 @@ APP_SECRET = "mFk56Ec74tSW74Dp8uQqmyxMHKVeMAgQU8cLjRFqV3Ro87cSiN"
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-twitter.search(q='python')
+pprint(twitter.search(q='python'))
+
