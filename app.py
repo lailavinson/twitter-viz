36a2376a8ff3000ac07728f0e33e98ecba034dcc
Richard Sun <richardsun29@ucla.edu>
2015-05-02 23:43:46 -0700

    create a json from the data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from twython import Twython
 from pprint import pprint
 from textblob import TextBlob
+import json
 
 APP_KEY = "Qn4Nx6C9LtoAlKtYLtoNQX2uY"
 APP_SECRET = "mFk56Ec74tSW74Dp8uQqmyxMHKVeMAgQU8cLjRFqV3Ro87cSiN"
@@ -8,11 +9,13 @@ APP_SECRET = "mFk56Ec74tSW74Dp8uQqmyxMHKVeMAgQU8cLjRFqV3Ro87cSiN"
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-json = twitter.search(q='python', count = 100)
+twitter_json = twitter.search(q='python', count = 100)
 
 data = []
-for status in json['statuses']:
+for status in twitter_json['statuses']:
     text = status['text']
     blob = TextBlob(text)
     dic = {'polarity': blob.sentiment.polarity, 'subjectivity': blob.sentiment.subjectivity }
     data.append(dic)
+
+JSON = json.dumps(data)
