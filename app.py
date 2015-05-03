ddfb28882d1142e2c4928c7259ed0b2d4ff7eb58
Richard Sun <richardsun29@ucla.edu>
2015-05-03 00:13:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import json
 
 APP_KEY = "Qn4Nx6C9LtoAlKtYLtoNQX2uY"
 APP_SECRET = "mFk56Ec74tSW74Dp8uQqmyxMHKVeMAgQU8cLjRFqV3Ro87cSiN"
-
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-twitter_json = twitter.search(q='python', count = 100)
 
+
+# Single query
+query = 'python'
+nTweets = 100
+
+twitter_json = twitter.search(q = query, count = nTweets)
 data = []
 for status in twitter_json['statuses']:
     text = status['text']
@@ -24,12 +28,11 @@ JSON = json.dumps(data)
 """ Multiple queries
 nTweets = 2
 queries = ['ucla', 'usc']
+
 twitter_jsons = []
 for query in queries:
     results = twitter.search(q = query, count = nTweets)
     twitter_jsons.append(results)
-    
-#twitter_json = twitter.search(q='python', count = 100)
 
 data = []
 for twitter_json in twitter_jsons:
