e35d637919799801c964dc30b9e85abe25a1bdfd
Richard Sun <richardsun29@ucla.edu>
2015-05-03 00:02:47 -0700

    add option for multiple queries

--- app.py
+++ app.py
@@ -19,3 +19,28 @@ for status in twitter_json['statuses']:
     data.append(dic)
 
 JSON = json.dumps(data)
+
+
+""" Multiple queries
+nTweets = 2
+queries = ['ucla', 'usc']
+twitter_jsons = []
+for query in queries:
+    results = twitter.search(q = query, count = nTweets)
+    twitter_jsons.append(results)
+    
+#twitter_json = twitter.search(q='python', count = 100)
+
+data = []
+for twitter_json in twitter_jsons:
+    curr_data = []
+    for status in twitter_json['statuses']:
+        text = status['text']
+        blob = TextBlob(text)
+        dic = {'polarity': blob.sentiment.polarity, 'subjectivity': blob.sentiment.subjectivity }
+        curr_data.append(dic)
+	wrapper = { twitter_json['search_metadata']['query']: curr_data }
+    data.append(wrapper)
+
+JSON = json.dumps(data)
+"""
